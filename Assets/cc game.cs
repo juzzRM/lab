f2477863d8fb3dead3e97e6e313f95e10b2abdb7
juzzRM <163971772+juzzRM@users.noreply.github.com>
2024-03-19 12:08:39 +0530

    game

---
+++ Assets/cc game.cs
@@ -0,0 +1,57 @@
+ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ccgame : MonoBehaviour
+{
+    public int speed;
+    public int score;
+    public int health = 3;
+    public int jumpforce;
+    private Rigidbody2D rigidbody;
+    public Text scoreText;
+    public AudioSource jumpSound;
+    public AudioSource coinSound;
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = 3;
+        rigidbody = GetComponent<Rigidbody2D>();
+        scoreText.text = "Score - " + score.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * speed * Time.deltaTime);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            jump();
+            jumpSound.Play();
+        }
+    }
+    void OnCollisionEnter2D(Collision2D colloider)
+    {
+        if (colloider.gameObject.tag == "coin")
+        {
+            score++;
+            coinSound.Play();
+            scoreText.text = "Score - " + score.ToString();
+            Destroy(colloider.gameObject);
+        }
+        else if (colloider.gameObject.tag == "spike")
+        {
+            health--;
+        }
+    }
+    void jump()
+    {
+        rigidbody.velocity = new Vector2(rigidbody.velocity.x, jumpforce);
+    }
+}
+
+
+
+
